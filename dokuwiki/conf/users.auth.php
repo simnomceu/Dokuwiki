6603aab3d1fd855f6d782354e5a9ed63fd6aa742
Isilin <casa2pir@hotmail.fr>
2018-05-27 15:23:28 +0000

    Refactoring.

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+isilin:$1$4UQ7DjK1$S9AyFCj3iMTcC6/8p/0i01:Isilin:casa2pir@hotmail.fr:admin,user
